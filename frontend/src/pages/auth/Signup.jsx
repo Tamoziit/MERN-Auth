491ed91eb0de5126249ec567cf05b2388dde51cd
Tamojit Das <tamojitdas181007@gmail.com>
2024-11-30 22:30:00 +0530

    Login UI

--- frontend/src/pages/auth/Signup.jsx
+++ frontend/src/pages/auth/Signup.jsx
@@ -22,7 +22,7 @@ const Signup = () => {
 			initial={{ opacity: 0, y: 20 }}
 			animate={{ opacity: 1, y: 0 }}
 			transition={{ duration: 0.5 }}
-			className="max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadeow-xl overflow-hidden"
+			className="max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden"
 		>
 			<div className="p-8">
 				<h2 className="text-3xl font-bold mb-6 text-center bg-gradient-to-r from-green-400 to-emerald-600 text-transparent bg-clip-text">
@@ -65,7 +65,7 @@ const Signup = () => {
 					</motion.button>
 				</form>
 			</div>
-			<div className="px-8 py-4 bg-gray-900 bg-opacity-50 flex justify-center">
+			<div className="px-8 py-4 bg-gray-800 bg-opacity-50 flex justify-center">
 				<p>
 					Already have an account?{" "}
 					<Link to={"/login"} className="text-green-400 hover:underline">Login</Link>
